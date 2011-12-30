b41cb45b9fa433e2465a3816d579322f3528537f
Michael Christenson II <michael@rebelhold.com>
2011-12-29 20:59:48 -0700

    added GPL3

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,21 @@
+# Rebel Foundation is a base rails app to start other apps from
+# Copyright (C) 2011 RebelHold
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# If you have any questions feel free to contact at: riseup@rebelhold.com
+
 require File.expand_path('../boot', __FILE__)
 
 require "action_controller/railtie"
